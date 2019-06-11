8840d9354197b68e221318d54bfde4de68c59ae7
Thibaud <33695800+tehret@users.noreply.github.com>
2019-06-11 17:52:58 +0200

    Update blind_denoising.py

--- blind_denoising.py
+++ blind_denoising.py
@@ -5,7 +5,7 @@
 # * All rights reserved.
 # *
 # * This program is free software: you can use, modify and/or
-# * redistribute it under the terms of the GNU General Public
+# * redistribute it under the terms of the GNU Affero General Public
 # * License as published by the Free Software Foundation, either
 # * version 3 of the License, or (at your option) any later
 # * version. You should have received a copy of this license along
